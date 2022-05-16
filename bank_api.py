00307edcb9a9e5c65f88685d9387e53850af22e2
cola <mgjeong@yonsei.ac.kr>
2022-05-16 16:28:13 -0500

    first commit!

---
+++ bank_api.py
@@ -0,0 +1,97 @@
+class Bank:
+    def __init__(self, name='Customer'):
+        self.data = dict()
+        self.name = name
+        
+    # card:pin = 1:1, card:account = 1:many, account:balance = 1:1
+    def create_card(self, card_id, PIN, account_id, balance=0):
+        self.data[card_id] = {'PIN':PIN, 'accounts':{account_id:balance}}
+        
+    def validate_PIN(self, card_id, PIN):
+        if card_id in self.data.keys() and self.data[card_id]['PIN'] == PIN :
+            return self.data[card_id]['accounts']
+        else:
+            return None
+        
+    def open_account(self, card_id, account_id, balance=0):
+        if card_id in self.data:
+            self.data[card_id]['accounts'][account_id] = balance
+        else:
+            return None
+    def close_account(self, card_id, account_id):
+        if card_id in self.data:
+            del self.data[card_id]['accounts'][account_id]
+        else:
+            return None
+    
+    def change_balance(self, card_id, account_id, flow):
+        if account_id in self.data[card_id]['accounts']:
+            self.data[card_id]['accounts'][account_id] += flow
+        else:
+            return None
+
+
+class ATMController:
+    
+    # Constructor of ATM with cash in
+    def __init__(self, bank_instance, cash):
+        self.bank = bank_instance
+        self.cash_bin = cash
+    
+    def insert_card(self, card_id, PIN):
+        self.myAccounts = self.bank.validate_PIN(card_id, PIN)
+        self.validity = 0
+        if self.myAccounts is None:
+            message = 'Invalid PIN number!'
+            return self.validity, message
+        else:
+            message = f'Welcome {self.bank.name}! Please select your account!'
+            self.validity = 1
+            return self.validity, message
+
+    def select_account(self, account_id):
+        if self.validity == 1:
+            if account_id in self.myAccounts:
+                return True
+            else:
+                message = f'{account_id} is not in your accounts!'
+                return False, message
+        else:
+            message = 'You have to enter valid PIN'
+            return False, message
+    
+    def implement_selection(self, card_id, account_id, selection, flow = 0):
+        if self.validity == 1:
+            if selection == 'Balance':
+                message = f'Your Balance is {self.myAccounts[account_id]}'
+                return None, message
+
+            elif selection == 'Deposit':
+                if flow > 0:
+                    self.bank.change_balance(card_id, account_id, flow)
+                    self.cash_bin += flow
+                    message = f'We got your Money! Your Balance is now {self.myAccounts[account_id]}'
+                    return None, message
+                else:
+                    return None, None
+
+            elif selection == 'Withdraw':
+                if flow > 0 and self.myAccounts[account_id] >= flow:
+                    if self.cash_bin >= flow:
+                        flow = - flow
+                        self.bank.change_balance(card_id, account_id, flow)
+                        message = f'Grab you Cash! Your Balance is now {self.myAccounts[account_id]}'
+                        return None, message
+                    else:
+                        message = 'Sorry! Not enough money in our bin!'
+                        return None, message
+                else:
+                    message = 'Not enough Balance in your account!'
+                    return None, message
+            else:
+                message = 'Invalid Action'
+                return None, message
+        else:
+            message = 'You have to enter valid PIN'
+            return None, message
+        
---
+++ test.py
@@ -0,0 +1,65 @@
+from bank_api import Bank, ATMController
+
+if __name__ == '__main__':
+    # Invalid Access
+    bank_s = Bank()
+    atm_s = ATMController(bank_s, 0)
+    flag, message = atm_s.insert_card('1234567890987654', '1234')
+    if flag == False:
+        print('Fail!', message)
+    else:
+        print('Success!', message)
+    
+    # Valid Access
+    bank_1 = Bank('Luke')
+    bank_1.create_card('1234567890987654', '1234', bank_1.name +'110000', balance=10000)
+    atm_1 = ATMController(bank_1, 10000)
+    validity, message = atm_1.insert_card('1234567890987654', '1234')
+    if validity == 0:
+        print('Fail!', message)
+    else:
+        print('Success!', message)
+    print()
+    
+    
+    # open and close accounts for card 1234567890987654
+    bank_1.open_account('1234567890987654', bank_1.name+'220000', balance=2000)
+    bank_1.open_account('1234567890987654', bank_1.name+'330000', balance=3000)
+    bank_1.open_account('1234567890987654', bank_1.name+'440000', balance=100000000)
+    bank_1.close_account('1234567890987654', bank_1.name+'220000')
+    _, message = atm_1.select_account(bank_1.name+'220000') 
+    print(message) # properly closed
+    print()
+    
+    # add new card to bank system
+    bank_1.create_card('9876543212345678', '4321', bank_1.name + '110001', balance=50)
+    bank_1.open_account('9876543212345678', bank_1.name+'220002', balance=20)
+    
+    
+    actions = [('Balance', 0), ("Withdraw", 5000), ('Deposit', 100000), ('Withdraw', 50000), ('Withdraw', 100000000), ('Save',1234)]
+    card_pin = [('1234567890987654', '1234') , ('1234567890987654', '4321'), ('9876543212345678', '4321')]
+    
+    # for 2 cards
+    print('Luke110000 will start with balance 10000')
+    for pair in card_pin:
+        num, pin = pair
+        print(f'*********This test is for {num}&{pin}*********')
+        validity, message = atm_1.insert_card(num, pin)
+        if validity == 0:
+            print('Fail!', message) # if wrong pin entered, print fail message
+            print()
+            continue
+        else:
+            print('Success!', message)
+            
+        for acc in bank_1.data[num]['accounts']:
+            print(f'***Test for account_id {acc}***')
+            for action in actions:
+                cmd, amt = action
+                _, message = atm_1.implement_selection(num, acc, cmd, amt)
+                print(message)
+        print('Test Successful!')
+        print()
+
+            
+    
